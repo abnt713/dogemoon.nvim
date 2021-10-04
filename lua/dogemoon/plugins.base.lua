ced9302d03263ddef1052cee612f2cb0fff88dfe
Alison Bento <alisonbnt@gmail.com>
2021-10-04 10:03:40 -0300

    create plugins base file

---
+++ lua/dogemoon/plugins.base.lua
@@ -0,0 +1,25 @@
+return {
+  'vim',
+  'paq-nvim',
+  'treesitter',
+  'polyglot',
+  'telescope',
+  'ale',
+  'colorizer',
+  'commentary',
+  'compe',
+  'editorconfig',
+  'git',
+  'indent',
+  'lsp',
+  'lspsaga',
+  'tmux',
+  'go',
+  'test',
+  'dap',
+
+  'nerdtree',
+
+  'theme.nord',
+  'lightline',
+}
